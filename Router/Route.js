4c9538082f78b598b8fc7e9fc2e0629136f06670
Mystigree <rjafiallah.rayan45@gmail.com>
2026-06-11 11:14:52 +0200

    Add authorize path for user statut accessibility pages.

--- Router/Route.js
+++ Router/Route.js
@@ -1,8 +1,9 @@
 export default class Route {
-    constructor(url, title, pathHTML, pathJS ="") {
-        this.url = url
-        this.title = title
-        this.pathHTML = pathHTML
-        this.pathJS = pathJS
+    constructor(url, title, pathHTML, authorize, pathJS ="") {
+        this.url = url;
+        this.title = title;
+        this.pathHTML = pathHTML;
+        this.pathJS = pathJS;
+        this.authorize = authorize;
     }
 }
--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,13 +1,13 @@
 import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/signin", "Se connecter", "/pages/auth/signin.html"),
-    new Route("/signup", "S'inscrire", "/pages/auth/signup.html"),
-    new Route("/avatars", "Avatars", "/pages/avatars.html"),
-    new Route("/contact", "Contact", "/pages/contact.html"),
-    new Route("/account", "Mon compte", "/pages/auth/account.html"),
-    new Route("/editPassword", "Modifier mon mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/", "Accueil", "/pages/home.html", []),
+    new Route("/signin", "Se connecter", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
+    new Route("/signup", "S'inscrire", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
+    new Route("/avatars", "Avatars", "/pages/avatars.html", []),
+    new Route("/contact", "Contact", "/pages/contact.html", []),
+    new Route("/account", "Mon compte", "/pages/auth/account.html", ["user", "admin", "employee"], "/js/auth/account.js"),
+    new Route("/editPassword", "Modifier mon mot de passe", "/pages/auth/editPassword.html", ["user", "admin", "employee"], "/js/auth/editPassword.js"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "PixelVerse Studio";
